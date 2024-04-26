4f9ba5bb7439b515f226db5afb2ce1f49e502b5f
LucasDelfrate <lucas.delfrate@docato.com.br>
2024-04-26 17:15:17 -0300

    Edição do front da tela home

--- src/view/HomeFrame.java
+++ src/view/HomeFrame.java
@@ -15,11 +15,16 @@ import javax.swing.JMenu;
 import javax.swing.JLabel;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Image;
+
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.border.BevelBorder;
+import javax.swing.JSplitPane;
+import javax.swing.JDesktopPane;
+import javax.swing.ImageIcon;
 
 public class HomeFrame extends JFrame {
 
@@ -49,34 +54,13 @@ public class HomeFrame extends JFrame {
 	public HomeFrame(Cliente cliente) {
 		this.cliente = cliente;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 657, 514);
+		setBounds(100, 100, 801, 685);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(128, 128, 128));
+		contentPane.setBackground(new Color(233, 233, 233));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JButton btnNewButton = new JButton("Login");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				LoginEmpresaFrame login = new LoginEmpresaFrame();
-				login.setVisible(true);
-			}
-		});
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnNewButton.setBounds(79, 222, 210, 46);
-		contentPane.add(btnNewButton);
-		
-		JButton btnLoginCandidato = new JButton("Login");
-		btnLoginCandidato.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				chamarLoginCandidato();
-			}
-		});
-		btnLoginCandidato.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnLoginCandidato.setBounds(354, 222, 210, 46);
-		contentPane.add(btnLoginCandidato);
 		
 		JButton btnRegistrar = new JButton("Registrar");
 		btnRegistrar.addActionListener(new ActionListener() {
@@ -84,39 +68,68 @@ public class HomeFrame extends JFrame {
 				chamarRegistroCandidato();
 			}
 		});
-		btnRegistrar.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnRegistrar.setBounds(354, 279, 210, 46);
+		btnRegistrar.setFont(new Font("Unispace", Font.PLAIN, 20));
+		btnRegistrar.setBounds(495, 386, 210, 46);
 		contentPane.add(btnRegistrar);
 		
-		JButton btnRegistrarEmpresa = new JButton("Registrar");
-		btnRegistrarEmpresa.addActionListener(new ActionListener() {
+		JButton btnLoginCandidato = new JButton("Login");
+		btnLoginCandidato.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				chamarLoginCandidato();
 			}
 		});
-		btnRegistrarEmpresa.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnRegistrarEmpresa.setBounds(79, 279, 210, 46);
-		contentPane.add(btnRegistrarEmpresa);
+		btnLoginCandidato.setFont(new Font("Unispace", Font.PLAIN, 20));
+		btnLoginCandidato.setBounds(495, 324, 210, 46);
+		contentPane.add(btnLoginCandidato);
 		
 		JLabel lblCandidato = new JLabel("Candidato");
 		lblCandidato.setHorizontalAlignment(SwingConstants.CENTER);
-		lblCandidato.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		lblCandidato.setBounds(354, 165, 210, 46);
+		lblCandidato.setFont(new Font("Unispace", Font.PLAIN, 25));
+		lblCandidato.setBounds(495, 250, 210, 46);
 		contentPane.add(lblCandidato);
 		
-		JLabel lblEmpresa = new JLabel("Empresa");
-		lblEmpresa.setHorizontalAlignment(SwingConstants.CENTER);
-		lblEmpresa.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		lblEmpresa.setBounds(79, 165, 210, 46);
-		contentPane.add(lblEmpresa);
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 121, 400, 525);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JButton btnNewButton_1 = new JButton("Login");
+		btnNewButton_1.setFont(new Font("Unispace", Font.PLAIN, 20));
+		btnNewButton_1.setBounds(98, 203, 210, 46);
+		panel.add(btnNewButton_1);
+		
+		JLabel lblEmpresa_1 = new JLabel("Empresa");
+		lblEmpresa_1.setForeground(new Color(255, 255, 255));
+		lblEmpresa_1.setBackground(new Color(221, 221, 221));
+		lblEmpresa_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblEmpresa_1.setFont(new Font("Unispace", Font.PLAIN, 25));
+		lblEmpresa_1.setBounds(98, 130, 210, 46);
+		panel.add(lblEmpresa_1);
+		
+		JButton btnRegistrarEmpresa_1 = new JButton("Registrar");
+		btnRegistrarEmpresa_1.setFont(new Font("Unispace", Font.PLAIN, 20));
+		btnRegistrarEmpresa_1.setBounds(98, 265, 210, 46);
+		panel.add(btnRegistrarEmpresa_1);
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setBounds(-40, -113, 635, 707);
+		panel.add(lblNewLabel_1);
+		
+		
+		Image img = new ImageIcon(this.getClass().getResource("/home_backgroung_medio.png")).getImage();
+		lblNewLabel_1.setIcon(new ImageIcon(img));
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(0, 0, 0));
+		panel_1.setBounds(0, 0, 792, 122);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("SIVAEM");
-		lblNewLabel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		lblNewLabel.setBackground(new Color(0, 0, 0));
-		lblNewLabel.setForeground(new Color(0, 0, 0));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Yu Gothic UI", Font.BOLD, 44));
-		lblNewLabel.setBounds(28, 11, 588, 78);
-		contentPane.add(lblNewLabel);
+		lblNewLabel.setForeground(new Color(243, 243, 243));
+		lblNewLabel.setFont(new Font("Unispace", Font.PLAIN, 40));
+		lblNewLabel.setBounds(314, 32, 163, 57);
+		panel_1.add(lblNewLabel);
 	
 	}
 	private void chamarLoginCandidato() {
--- src/view/RegistroCandidatoFrame.java
+++ src/view/RegistroCandidatoFrame.java
@@ -37,7 +37,7 @@ public class RegistroCandidatoFrame extends JFrame {
 	public RegistroCandidatoFrame(HomeFrame home, Cliente cliente) {
 		this.home = home;
 		this.cliente = cliente;
-		setBounds(100, 100, 664, 499);
+		setBounds(100, 100, 774, 499);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -48,17 +48,10 @@ public class RegistroCandidatoFrame extends JFrame {
 		contentPane_1.setLayout(null);
 		contentPane_1.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane_1.setBackground(Color.GRAY);
-		contentPane_1.setBounds(0, 0, 648, 469);
+		contentPane_1.setBounds(0, 0, 542, 469);
 		contentPane.add(contentPane_1);
 		
-		JLabel lblLoginCandidato = new JLabel("Registro Candidato");
-		lblLoginCandidato.setHorizontalAlignment(SwingConstants.CENTER);
-		lblLoginCandidato.setForeground(new Color(0, 0, 0));
-		lblLoginCandidato.setFont(new Font("Verdana", Font.PLAIN, 44));
-		lblLoginCandidato.setBounds(0, 0, 648, 125);
-		contentPane_1.add(lblLoginCandidato);
-		
-		JButton btnRegistrar_1 = new JButton("REGISTRAR");
+		JButton btnRegistrar_1 = new JButton("Registrar");
 		btnRegistrar_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Candidato candidato = new Candidato();
@@ -72,41 +65,49 @@ public class RegistroCandidatoFrame extends JFrame {
 				 registrarCandidato(res);
 			}
 		});
-		btnRegistrar_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnRegistrar_1.setBounds(217, 341, 211, 46);
+		btnRegistrar_1.setFont(new Font("Unispace", Font.PLAIN, 15));
+		btnRegistrar_1.setBounds(118, 362, 211, 29);
 		contentPane_1.add(btnRegistrar_1);
 		
 		JLabel lblNewLabel = new JLabel("Nome");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblNewLabel.setBounds(290, 120, 57, 14);
+		lblNewLabel.setFont(new Font("Unispace", Font.PLAIN, 13));
+		lblNewLabel.setBounds(118, 148, 57, 14);
 		contentPane_1.add(lblNewLabel);
 		
 		iptNome = new JTextField();
-		iptNome.setBounds(217, 145, 214, 29);
+		iptNome.setBounds(118, 167, 214, 29);
 		contentPane_1.add(iptNome);
 		iptNome.setColumns(10);
 		
 		iptEmail = new JTextField();
 		iptEmail.setColumns(10);
-		iptEmail.setBounds(217, 210, 214, 29);
+		iptEmail.setBounds(118, 232, 214, 29);
 		contentPane_1.add(iptEmail);
 		
 		JLabel lblEmail = new JLabel("Email");
-		lblEmail.setHorizontalAlignment(SwingConstants.CENTER);
-		lblEmail.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblEmail.setBounds(290, 185, 57, 14);
+		lblEmail.setFont(new Font("Unispace", Font.PLAIN, 13));
+		lblEmail.setBounds(118, 207, 57, 14);
 		contentPane_1.add(lblEmail);
 		
 		JLabel lblSenha = new JLabel("Senha");
-		lblSenha.setHorizontalAlignment(SwingConstants.CENTER);
-		lblSenha.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblSenha.setBounds(290, 250, 59, 14);
+		lblSenha.setFont(new Font("Unispace", Font.PLAIN, 13));
+		lblSenha.setBounds(118, 272, 59, 14);
 		contentPane_1.add(lblSenha);
 		
 		iptSenha = new JPasswordField();
-		iptSenha.setBounds(217, 275, 214, 29);
+		iptSenha.setBounds(118, 297, 214, 29);
 		contentPane_1.add(iptSenha);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 542, 95);
+		contentPane_1.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Registro candidato");
+		lblNewLabel_1.setBounds(163, 35, 216, 25);
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setFont(new Font("Unispace", Font.PLAIN, 20));
+		panel.add(lblNewLabel_1);
 	}
 	public void registrarCandidato(JSONObject res) {
 		if(this.cliente == null) {
